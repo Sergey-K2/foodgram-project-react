28575edceb84bc463447847551f2d48b5798c905
Sergey Kozlov <serand.kozlov@gmail.com>
2023-04-23 16:17:45 +0300

    Third review

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -83,7 +83,7 @@ class IngredientRecipe(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     amount = models.PositiveSmallIntegerField(
-        verbose_name="Количество", default="have your default amount id here"
+        verbose_name="Количество", default="default amount"
     )
 
 
