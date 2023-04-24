5cab4e59f2c1ce48a99d794ca4e9778fb13601fe
Sergey Kozlov <serand.kozlov@gmail.com>
2023-04-24 08:12:31 +0300

    Third review

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -2,16 +2,20 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (IngredientViewSet, RecipeViewSet, SubscriptionViewSet,
-                    TagViewSet)
+from .views import (
+    IngredientViewSet,
+    RecipeViewSet,
+    SubscriptionViewSet,
+    TagViewSet,
+)
 
 app_name = "api"
 
 v1_router = DefaultRouter()
 
-v1_router.register("recipes", RecipeViewSet, basename="recipes")
-v1_router.register("tag", TagViewSet, basename="tags")
-v1_router.register("ingredients", IngredientViewSet, basename="ingredients")
+v1_router.register("recipes", RecipeViewSet)
+v1_router.register("tag", TagViewSet)
+v1_router.register("ingredients", IngredientViewSet)
 v1_router.register(
     "subscriptions", SubscriptionViewSet, basename="subscriptions"
 )
