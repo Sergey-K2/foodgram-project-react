10f9ad8c3bf2f248dd48a8d304939d6309aae955
Sergey Kozlov <serand.kozlov@gmail.com>
2023-04-26 10:05:57 +0300

    Fourth review

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,12 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (
-    IngredientViewSet,
-    RecipeViewSet,
-    TagViewSet,
-    UsersSubscriptionViewSet,
-)
+from .views import (IngredientViewSet, RecipeViewSet, TagViewSet,
+                    UsersSubscriptionViewSet)
 
 app_name = "api"
 
