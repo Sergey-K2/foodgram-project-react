11f77d4e5016eb9f21370522dd6c580472ba0cc1
Sergey Kozlov <serand.kozlov@gmail.com>
2023-04-26 08:29:19 +0300

    Fourth review

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -2,17 +2,9 @@ import base64
 
 from django.conf import settings
 from django.core.files.base import ContentFile
-from recipes.models import (
-    Favorite,
-    Ingredient,
-    IngredientRecipe,
-    Recipe,
-    ShoppingCart,
-    Subscription,
-    Tag,
-    User,
-)
 from djoser.serializers import UserCreateSerializer, UserSerializer
+from recipes.models import (Favorite, Ingredient, IngredientRecipe, Recipe,
+                            ShoppingCart, Subscription, Tag, User)
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from rest_framework.validators import UniqueTogetherValidator
