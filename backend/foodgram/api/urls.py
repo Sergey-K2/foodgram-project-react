0a57cec4689beec6996e4c43e542c1e1e5a3248f
Sergey Kozlov <serand.kozlov@gmail.com>
2023-04-26 09:55:41 +0300

    Fourth review

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -2,8 +2,12 @@ from django.urls import include, path
 from djoser.views import TokenCreateView, TokenDestroyView
 from rest_framework.routers import DefaultRouter
 
-from .views import (IngredientViewSet, RecipeViewSet, TagViewSet,
-                    UsersSubscriptionViewSet)
+from .views import (
+    IngredientViewSet,
+    RecipeViewSet,
+    TagViewSet,
+    UsersSubscriptionViewSet,
+)
 
 app_name = "api"
 
@@ -17,6 +21,7 @@ v1_router.register("users", UsersSubscriptionViewSet, basename="subscriptions")
 urlpatterns = [
     path("", include(v1_router.urls)),
     path("", include("djoser.urls")),
-    path("auth/token/login/", TokenCreateView.as_view(), name="login"),
-    path("auth/token/logout/", TokenDestroyView.as_view(), name="logout"),
+    path(
+        "auth/", include('djoser.urls.authtoken'))
+    ),
 ]
