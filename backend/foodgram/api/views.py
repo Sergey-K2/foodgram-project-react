e769bccf8413c9449f5254b0b681d47dff9327d8
Sergey Kozlov <serand.kozlov@gmail.com>
2023-05-03 13:29:24 +0300

    Fifth review

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -3,8 +3,16 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
-from recipes.models import (CustomUser, Favorite, Ingredient, IngredientRecipe,
-                            Recipe, ShoppingCart, Subscription, Tag)
+from recipes.models import (
+    CustomUser,
+    Favorite,
+    Ingredient,
+    IngredientRecipe,
+    Recipe,
+    ShoppingCart,
+    Subscription,
+    Tag,
+)
 from rest_framework import exceptions, filters, mixins
 from rest_framework.decorators import action
 from rest_framework.pagination import LimitOffsetPagination
@@ -14,10 +22,15 @@ from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
 from .filters import IngredientFilter, RecipeFilter
 from .permissions import AuthenticatedOrAuthorOrReadOnly
-from .serializers import (CreateUpdateRecipeSerializer, CustomUserSerializer,
-                          IngredientSerializer, RecipeLimitedSerializer,
-                          RecipeSerializer, SubscriptionSerializer,
-                          TagSerializer)
+from .serializers import (
+    CreateUpdateRecipeSerializer,
+    CustomUserSerializer,
+    IngredientSerializer,
+    RecipeLimitedSerializer,
+    RecipeSerializer,
+    SubscriptionSerializer,
+    TagSerializer,
+)
 
 
 class ListRetrieveViewSet(
@@ -110,6 +123,7 @@ class RecipeViewSet(ModelViewSet):
             serializer = RecipeLimitedSerializer(
                 recipe, context={"request": request}, many=True
             )
+            return Response(serializer.data)
 
     @action(detail=False, permission_classes=(IsAuthenticated,))
     def download_shopping_cart(self, request):
